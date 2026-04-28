ea66a732f4dbb31eddafb22bb4c34b39f2603a4a
Abru786 <uddinabrar360@gmail.com>
2026-04-28 14:41:18 +0530

    added and implemented logging with logback and slf4j

--- src/main/java/in/ms/ORDER_SERVICE/controller/OrderController.java
+++ src/main/java/in/ms/ORDER_SERVICE/controller/OrderController.java
@@ -4,6 +4,8 @@ import in.ms.ORDER_SERVICE.dto.OrderRequest;
 import in.ms.ORDER_SERVICE.entity.Order;
 import in.ms.ORDER_SERVICE.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +16,36 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderController {
 
+
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     private final OrderService orderService;
 
     @PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody OrderRequest request) {
+
+        log.info("Creating order for userId: {}", request.getUserId());
         return ResponseEntity.ok(orderService.createOrder(request));
     }
     @GetMapping("/{id}")
     public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+
+        log.info("Fetching order with id: {}", id);
         return ResponseEntity.ok(orderService.getOrderById(id));
     }
 
     // ✅ Get all orders
     @GetMapping("/all")
     public ResponseEntity<List<Order>> getAllOrders() {
+        log.info("Fetching all orders");
         return ResponseEntity.ok(orderService.getAllOrders());
     }
 
     // ✅ Get orders by user
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Order>> getOrdersByUser(@PathVariable Long userId) {
+
+        log.info("Fetching orders for userId: {}", userId);
         return ResponseEntity.ok(orderService.getOrdersByUser(userId));
     }
 }
